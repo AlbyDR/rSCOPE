70073f8583f4b90f176ca3ff6f945354c46012ed
AlbyDR <albyduarte@gmail.com>
2021-11-03 18:11:54 +0100

    filter accuracy

--- R/get_accuracy.R
+++ R/get_accuracy.R
@@ -35,8 +35,105 @@
 get_accuracy <- function(
   obs_vec,
   predictions,
-  metric_function
+  metric_function,
+  Filter = FALSE,
+  timestamp,
+  month_start = 1,
+  month_end = 12,
+  period = c("day", "dawn_dusk", "night"),
+  nu_interations,
+  interations = 101,
+  dryhours_vec,
+  dry_hours = 0,
+  neg_vec,
+  neg_null = 0,
+  lat = 52.46,
+  lon = 13.32
 ){
+
+
+  if(Filter == TRUE){
+
+    predictions_df <- predictions
+
+    interations_NA <- tibble::tibble(data.frame(sapply(1:length(predictions), function(i) ifelse(nu_interations[i] <= interations, 1, NA))))
+
+    predictions_df <- interations_NA*predictions_df
+
+    night <- suncalc::getSunlightTimes(date = lubridate::date(timestamp), lat = lat, lon = lon, tz = "UTC")
+
+    night$time <- timestamp
+    night$sun <- NULL
+    night$sun[night$time>night$nadir] <- "Night" ##seting
+    night$sun[night$time>night$night & hour(night$time)<hour(night$nadir)] <- "Night"    #rising
+    night$sun[night$time>night$nightEnd & night$time<night$nauticalDawn] <- "Dawn" #Morn.Astr.night
+    night$sun[night$time>night$nauticalDawn & night$time<night$dawn] <- "Dawn"     #Morn.Nau.ECsun
+    night$sun[night$time>night$dawn & night$time<night$sunrise] <- "Dawn"          #Morn.Civil.ECsun
+    night$sun[night$time>night$sunrise & night$time<night$goldenHourEnd] <- "Goldenhour.M"
+    night$sun[night$time>night$goldenHourEnd & night$time<night$solarNoon] <- "Rising"
+    night$sun[night$time>night$goldenHourEnd & hour(night$time)<(hour(night$solarNoon)-2)] <- "Rising"
+    night$sun[hour(night$time)==round(hour(night$solarNoon))] <- "Noon"
+    night$sun[night$time>night$solarNoon & night$time<night$goldenHour] <- "Setting"
+    night$sun[night$time>night$goldenHour & night$time<night$sunset] <- "Goldenhour.A"
+    night$sun[night$time>night$sunset & night$time<night$dusk] <- "Dusk"        #Even.Civil.ECsun
+    night$sun[night$time>night$dusk & night$time<night$nauticalDusk] <- "Dusk"  #Even.Nau.ECsun
+    night$sun[night$time>night$nauticalDusk & night$time<night$night] <- "Dusk" #Even.Astr.ECsun
+
+    night$daynight <- NULL
+    night$daynight[night$sun == "Setting"] <- "day"
+    night$daynight[night$sun == "Noon"] <- "day"
+    night$daynight[night$sun == "Rising"] <- "day"
+    night$daynight[night$sun == "Dusk"] <- "dawn_dusk"
+    night$daynight[night$sun == "Dawn"] <- "dawn_dusk"
+    night$daynight[night$sun == "Night"] <- "night"
+    night$daynight[night$sun == "Goldenhour.M"] <-"day"
+    night$daynight[night$sun == "Goldenhour.A"] <- "day"
+
+    day_night <- night$daynight
+
+    obs_vec <- data.frame("timestamp" = timestamp,
+                          "obs_vec" = obs_vec,
+                          "dryhours_vec" = dryhours_vec,
+                          "day_night" = day_night,
+                          "neg_vec" = neg_vec)
+
+    filtered <- dplyr::filter(obs_vec, lubridate::month(timestamp) >= month_start & lubridate::month(timestamp) <= month_end)
+
+    filtered <- dplyr::filter(filtered, day_night %in% period)
+
+    filtered <- dplyr::filter(filtered, dryhours_vec >= dry_hours)
+
+    filtered <- dplyr::filter(filtered, neg_vec >= neg_null)
+
+    predictions_df$timestamp <- timestamp
+
+    predictions_df <- dplyr::left_join(filtered[,1:2], predictions_df, by = "timestamp")
+
+    names(predictions_df) <- c("timestamp", "obs_vec", rep("pred", length(predictions)))
+
+    obs_vec <- filtered[,2]
+
+    Predictions_metrics <- lapply(1:length(predictions), function(i) metric_function(
+      predictions_df[i+2],
+      truth = obs_vec,
+      estimate = "pred", na_rm = TRUE))
+
+    metrics_table <- data.frame(t(sapply(1:(length(predictions)), function(i) Predictions_metrics[[i]]$.estimate)))
+    colnames(metrics_table) <- Predictions_metrics[[1]]$.metric
+    rownames(metrics_table) <- names(predictions)
+
+    metrics_table$rBias <- sapply(1:length(predictions), function(i) sum(predictions_df[i+1]-obs_vec, na.rm = T)/
+                                    sum(obs_vec, na.rm = T))
+
+    names(predictions_df) <- c("timestamp", "obs_vec", names(predictions))
+
+    metrics_table$n_obs <- sapply(1:length(predictions), function(i) length(filter(predictions_df, !is.na(obs_vec) &
+                                                                                                   !is.na(predictions_df[i+2]))[,i+2]))
+
+
+  }else{
+
+
   predictions_df <- predictions
   names(predictions_df) <- rep("pred",length(predictions_df))
 
@@ -52,6 +149,10 @@ get_accuracy <- function(
   metrics_table$rBias <- sapply(1:length(predictions), function(i) sum(predictions[i]-obs_vec, na.rm = T)/
                                   sum(obs_vec, na.rm = T))
 
+  metrics_table$n_obs <- sapply(1:length(predictions), function(i) matrix(table(is.na(obs_vec)))[1])
+  }
+
   return(metrics_table)
+
 
 }
